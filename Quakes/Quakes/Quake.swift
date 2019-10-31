3f1a2df132caed13cd020663831b908da2023edb
Jordan Christensen <jordan.c4922@gmail.com>
2019-11-01 01:31:09 +0800

    Add all needed variables to model, add tests

--- Quakes/Quakes/Quake.swift
+++ Quakes/Quakes/Quake.swift
@@ -9,5 +9,37 @@
 import Foundation
 
 class Quake: NSObject, Decodable {
+    let magnitude: Double
+    let place: String
+    let time: Date
+    let latitude: Double
+    let longitude: Double
     
+    enum QuakeCodingKeys: String, CodingKey {
+        case magnitude = "mag"
+        case place
+        case time
+        case properties
+        
+        case latitude
+        case longitude
+        case geometry
+        case coordinates
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: QuakeCodingKeys.self)
+        let properties = try container.nestedContainer(keyedBy: QuakeCodingKeys.self, forKey: .properties)
+        let geometry = try container.nestedContainer(keyedBy: QuakeCodingKeys.self, forKey: .geometry)
+        var coordinates = try geometry.nestedUnkeyedContainer(forKey: .coordinates)
+        
+        self.magnitude = try properties.decode(Double.self, forKey: .magnitude)
+        self.place = try properties.decode(String.self, forKey: .place)
+        self.time = try properties.decode(Date.self, forKey: .time)
+        
+        self.longitude = try coordinates.decode(Double.self)
+        self.latitude = try coordinates.decode(Double.self)
+        
+        super.init()
+    }
 }
--- Quakes/QuakesTests/QuakesTests.swift
+++ Quakes/QuakesTests/QuakesTests.swift
@@ -16,6 +16,20 @@ class QuakesTests: XCTestCase {
     }
 
     func testFail() {
-        XCTFail()
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        let date = Date(timeIntervalSince1970: 1388620296020 / 1000.0)
+        do {
+            let quake = try decoder.decode(Quake.self, from: quakeData)
+            XCTAssertEqual(1.29, quake.magnitude, accuracy: 0.001)
+            
+            XCTAssertEqual("10km SSW of Idyllwild, CA", quake.place)
+            XCTAssertEqual(date, quake.time)
+            
+            XCTAssertEqual(33.663333299999998, quake.latitude, accuracy: 0.001)
+            XCTAssertEqual(-116.7776667, quake.longitude, accuracy: 0.001)
+        } catch {
+            XCTFail("Error decoding! \(error)")
+        }
     }
 }
